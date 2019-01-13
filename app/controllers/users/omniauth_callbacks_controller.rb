65e1ef8e8dcc9c4d3324d6495588e3746f0da6e2
lime1024 <lime1024@gmail.com>
2019-01-13 11:25:14 +0900

    Move from my_web_apps

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  # You should configure your model like this:
+  # devise :omniauthable, omniauth_providers: [:twitter]
+
+  # You should also create an action method in this controller like this:
+  # def twitter
+  # end
+
+  # More info at:
+  # https://github.com/plataformatec/devise#omniauth
+
+  # GET|POST /resource/auth/twitter
+  # def passthru
+  #   super
+  # end
+
+  # GET|POST /users/auth/twitter/callback
+  # def failure
+  #   super
+  # end
+
+  # protected
+
+  # The path used when OmniAuth fails
+  # def after_omniauth_failure_path_for(scope)
+  #   super(scope)
+  # end
+  def github
+    # You need to implement the method below in your model
+    @user = User.find_for_github_oauth(request.env["omniauth.auth"], current_user)
+
+    if @user.persisted?
+      set_flash_message(:notice, :success, kind: "GitHub") if is_navigational_format?
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session["devise.github_data"] = request.env["omniauth.auth"].except("extra")
+      redirect_to new_user_registration_url
+    end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    confirmations: "users/confirmations",
+    passwords:     "users/passwords",
+    registrations: "users/registrations",
+    sessions:      "users/sessions",
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
+  root "books#index"
+  resources :user, only: [:new, :create]
+
+  resources :books
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
